60409917916c18ee334696ca771714b473b471fc
demoncowgirl <bethsalvatore2018@gmail.com>
2019-08-04 19:40:48 -0500

    first commit

---
+++ resources/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    @yield('styles')
+  </head>
+  <body>
+    @yield('content')
+  </body>
+  @yieldd('scripts')
+</html>
