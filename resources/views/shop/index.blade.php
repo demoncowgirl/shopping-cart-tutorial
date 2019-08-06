0e3f171495a00e3d8cd1c6682f6d3c4e002c2c68
demoncowgirl <bethsalvatore2018@gmail.com>
2019-08-06 17:27:55 -0500

    adds navbar and starts grid for index page

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -2,6 +2,15 @@
 @section('title')
 Laravel Shopping Cart
 @endsection
-@section('contents')
-<h1>It works!</h1>
+@section('content')
+<div class="row">
+  <span class="border border-secondary">
+  <div class="col-sm-6 p-3">
+    <img src="...." alt="....">
+    <h3>Thumbnail label</h3>
+    <p>stuff here</p>
+    <p><a href="#" class= "btn btn-primary" role="button">Button</a><a href="#" class= "btn btn-default" role="button">Button</a></p>
+  </div>
+</span>
+</div>
 @endsection
